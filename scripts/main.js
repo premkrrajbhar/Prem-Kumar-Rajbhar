577f5bfcaffe7d1700df8957ad9f10ca5055ddc2
premkrrajbhar <premkumarrajbhar22@gmail.com>
2026-08-28 01:15:29 +0530

    logic for splash message

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,29 @@ AOS.init({
   once: true,
 });
 
+const splash = document.getElementById("helloSplash");
+const words = document.querySelectorAll(".hello-word");
+
+let current = 0;
+
+const interval = setInterval(() => {
+    words[current].classList.remove("active");
+
+    current++;
+
+    if (current < words.length) {
+        words[current].classList.add("active");
+    } else {
+        clearInterval(interval);
+
+        splash.classList.add("hide");
+
+        setTimeout(() => {
+            splash.remove();
+        }, 800);
+    }
+}, 350);
+
 const offcanvas = document.getElementById("offcanvasExample");
 const toggleBtn = document.querySelector(".menu-toggle");
 
